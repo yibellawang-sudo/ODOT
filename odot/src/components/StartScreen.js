cf3a1997c14d2359f8d21c79d37c26a007f848da
Solane Dasha <dashasolane430@gmail.com>
2025-11-15 19:30:55 -0800

    app

--- odot/src/components/StartScreen.js
+++ odot/src/components/StartScreen.js
@@ -1,25 +1,24 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import startImg from "../images/onboarding-removebg-preview.png"; 
-
+import logoImg from "../images/odotlogotrans.png"; 
 function StartScreen() {
   const navigate = useNavigate();
 
   return (
     <div className="body">
-      <p>! trigger warning !</p>
-      <h1 className="App-header">odot</h1>
-
+      
+      <h1 className="App-header">Welcome to</h1>
+<img src={logoImg} alt="logo"/>
       <button className="button" onClick={() => navigate("./onboarding")}>
         start!
       </button>
 
-      {/* Image button */}
       <button onClick={() => navigate("./onboarding")}>
         <img 
           src={startImg} 
           alt="Button" 
-          style={{ width: "120px", cursor: "pointer" }}
+          style={{ width: "300px", cursor: "pointer" }}
         />
       </button>
     </div>
